0853c3744e550e54b7f929058107839bd08251c3
Russ Parmer <rparmer02@gmail.com>
2024-05-04 11:11:36 -0700

    add unifi parental controls

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,42 @@
-resource "proxmox_vm_qemu" "k3s_nodes" {
-  count = var.k3s_node_count
+module "puppet-1" {
+  source = "./modules/proxmox_vm"
 
-  name        = "k3s-hans-${count.index + 1}"
-  desc        = "k3s-hans-${count.index + 1}"
-  target_node = "gaston"
-
-  clone = "ubuntu-cloud"
-
-  os_type   = "cloud-init"
-  qemu_os   = "other"
-  ipconfig0 = "ip=192.168.2.1${format("%02d", count.index + 1)}/22,gw=192.168.1.1"
-
-  cores  = 4
-  memory = 1024 * 16
-
-  scsihw = "virtio-scsi-pci"
-  onboot = true
-
-  disk {
-    storage = "local-lvm"
-    type    = "scsi"
-    size    = "30G"
-  }
-
-  lifecycle {
-    ignore_changes = [
-      sshkeys,
-      network,
-    ]
-  }
-
-  #   provisioner "local-exec" {
-  #     command = "sleep 30 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u knox -i '10.0.4.${count.index + 1},' playbook.yml"
-  #   }
+  name = "puppet-1"
+  ip   = "192.168.2.11/22"
 }
+# resource "proxmox_vm_qemu" "k3s_nodes" {
+#   count = var.k3s_node_count
+
+#   name        = "k3s-hans-${count.index + 1}"
+#   desc        = "k3s-hans-${count.index + 1}"
+#   target_node = "gaston"
+
+#   clone = "ubuntu-cloud"
+
+#   os_type   = "cloud-init"
+#   qemu_os   = "other"
+#   ipconfig0 = "ip=192.168.2.1${format("%02d", count.index + 1)}/22,gw=192.168.1.1"
+
+#   cores  = 2
+#   memory = 1024 * 2
+
+#   scsihw = "virtio-scsi-pci"
+#   onboot = true
+
+#   disk {
+#     storage = "local-lvm"
+#     type    = "scsi"
+#     size    = "30G"
+#   }
+
+#   lifecycle {
+#     ignore_changes = [
+#       sshkeys,
+#       network,
+#     ]
+#   }
+
+#   #   provisioner "local-exec" {
+#   #     command = "sleep 30 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u knox -i '10.0.4.${count.index + 1},' playbook.yml"
+#   #   }
+# }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,13 +2,16 @@ terraform {
   required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
-      version = ">= 2.9.14"
+      version = "3.0.1-rc1"
     }
   }
 }
 
 provider "proxmox" {
-  pm_api_url = "https://192.168.2.1:8006/api2/json"
+  pm_api_url      = "https://192.168.2.10:8006/api2/json"
+  pm_tls_insecure = true
+  # pm_user = "root@pve"
+  # pm_password = "B@ndit0920"
 
   # set PM_USER and PM_PASS env vars
 }
