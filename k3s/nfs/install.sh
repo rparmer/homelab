39ebec6ad0bd7cc63e51e0bdb60bd3b3f6325e08
Russ Parmer <rparmer02@gmail.com>
2024-06-28 20:07:24 -0700

    remove namespace from cilium pools

--- k3s/nfs/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
-
-# helm repo update
-helm -n kube-system upgrade -i nfs-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner -f nfs.values.yaml
--- k3s/nfs/uninstall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-helm -n kube-system uninstall nfs-provisioner
